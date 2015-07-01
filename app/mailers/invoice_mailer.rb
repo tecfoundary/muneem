cf2f8abda1ec245d21243bed6503eb8655293006
Gaurav Cheema <gcheema@tecfoundary.com>
2015-07-02 00:40:53 +1000

    Model Serialization issue, so using id in worker job

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,10 +1,10 @@
 class InvoiceMailer < ApplicationMailer
   layout 'invoice'
 
-  def email_invoice(invoice, user)
-    @invoice = invoice
+  def email_invoice(invoice_id, user_id, account_id)
+    @invoice = Invoice.where(id: invoice_id, account: account_id).first
     @client = @invoice.client
-    @user = user
+    @user = User.where(id: user_id, account: account_id)
     mail(from: @user.email, to: @client.email, subject: "Invoice #{@invoice} from #{@invoice.account.name} for #{@client.carer}" )
   end
 
