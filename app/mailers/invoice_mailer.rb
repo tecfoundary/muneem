fa9ac5acd16c512447b8e262662533d7b8b02773
Gaurav Cheema <gcheema@tecfoundary.com>
2015-08-23 17:32:27 +1000

    Minor fix

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,11 +1,11 @@
 class InvoiceMailer < ApplicationMailer
   layout 'invoice'
 
-  def email_invoice(invoice_id, user_id, account_id)
-    @invoice = Invoice.where(id: invoice_id, account: account_id).first
+  def email_invoice(invoice, user, account_id)
+    @invoice = invoice
     @client = @invoice.client
-    @user = User.where(id: user_id, account: account_id).first
-    mail(reply_to: @user.email, to: @client.email, subject: "Invoice #{@invoice} from #{@invoice.account.name} for #{@client.carer}" )
+    @user = user
+    mail(from: ENV['DEFAULT_FROM_ADDRESS'], reply_to: @user.email, to: @client.email, subject: "Invoice #{@invoice} from #{@invoice.account.name} for #{@client.carer}" )
   end
 
 end
